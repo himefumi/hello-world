9b2920637887145cd40a84b07dfcfdc704f6a06b
g10913hf <g10913hf@s30457.tsuda.ac.jp>
2014-04-11 11:03:46 +0900

    hello,worldと表示します

---
+++ src/hello/world/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world;
+
+/**
+ *
+ * @author g10913hf
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hwllo,world");
+    }
+}
